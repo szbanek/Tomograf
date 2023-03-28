f4c5ced179987c34660de9bf928aa4d7b072951a
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2023-03-28 18:03:30 +0200

    convert dicom to static class

--- dicom.py
+++ dicom.py
@@ -5,28 +5,28 @@ from datetime import date
 
 
 class Dicom:
-    def load_image(self, dicom_file):
+    def load_image(dicom_file):
         img = dicom_file.pixel_array.astype(float)
         rescaled_img = (np.maximum(img, 0) / img.max()) * 255
         final_img = np.uint8(rescaled_img)
         final_img = Image.fromarray(final_img)
         return final_img
 
-    def load_file(self, path):
+    def load_file(path):
         ds = pydicom.dcmread(path)
         patientData = {}
         patientData['PatientName'] = ds['PatientName'].value
         patientData['StudyDate'] = ds['StudyDate'].value
         patientData['StudyDescription'] = ds['StudyDescription'].value
-        patientData['Image'] = self.load_image(ds)
+        patientData['Image'] = Dicom.load_image(ds)
         return patientData
 
-    def save_file(self, patientData, path):
+    def save_file(patientData, path):
         img = np.asarray(patientData['Image'])
         ds = pydicom.dcmread('Input/Dicom/dicom.dcm')
 
         ds.PatientName = patientData['PatientName']
-        ds.StudyDate = self.convert_date(patientData['StudyDate'])
+        ds.StudyDate = Dicom.convert_date(patientData['StudyDate'])
         ds.StudyDescription = patientData['StudyDescription']
         ds.PixelData = patientData['Image'].tobytes()
 
@@ -40,15 +40,14 @@ class Dicom:
 
         ds.save_as(path)
 
-    def convert_date(self, date):
+    def convert_date(date):
         return date.strftime('%Y%m%d')
 
 
 if __name__ == '__main__':
-    ds = Dicom()
     # Loading from dicom file example
     path = 'Input/Dicom/dicom.dcm'
-    patientData = ds.load_file(path)
+    patientData = Dicom.load_file(path)
     img = patientData['Image']
     #img.save('original_img.jpg')
 
@@ -57,4 +56,4 @@ if __name__ == '__main__':
     img = Image.open('Input/Img/Kwadraty2.jpg').convert('L')
     patientData = {'PatientName': 'Jakub Czarnecki', 'StudyDescription': 'TK Glowy', 'Image': img,
                    'StudyDate': studyDate}
-    ds.save_file(patientData, 'Output/Dicom/new_dicom.dcm')
+    Dicom.save_file(patientData, 'Output/Dicom/new_dicom.dcm')
